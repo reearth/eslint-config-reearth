fc2e0c7676645953b59d31c0f69bac53f7166a0d
rot1024 <aayhrot@gmail.com>
2022-07-16 03:46:08 +0900

    fix: disable no-missing-import in node config

--- node-commonjs.js
+++ node-commonjs.js
@@ -1,6 +1,8 @@
 module.exports = {
   extends: ["./node", "./commonjs"],
   rules: {
+    "node/no-missing-import": "error",
+    "node/no-missing-require": "error",
     "node/no-unsupported-features/es-syntax": ["error", { ignores: [] }],
   },
 };
--- node.js
+++ node.js
@@ -5,8 +5,10 @@ module.exports = {
   extends: ["plugin:node/recommended"],
   plugins: ["node"],
   rules: {
-    "node/no-unpublished-require": "off",
+    "node/no-missing-import": "off",
+    "node/no-missing-require": "off",
     "node/no-unpublished-import": "off",
+    "node/no-unpublished-require": "off",
     "node/no-unsupported-features/es-syntax": ["error", { ignores: ["modules"] }],
   },
 };
