d19de4070a0df59c965c59535c5def38d18bfe04
rot1024 <aayhrot@gmail.com>
2022-07-25 16:34:41 +0900

    fix: remove some rules from test

--- test.js
+++ test.js
@@ -1,25 +1,4 @@
 module.exports = {
   extends: [require.resolve("./node")],
   plugins: ["testing-library"],
-  rules: {
-    "no-restricted-globals": [
-      "error",
-      {
-        name: "expect",
-        message: 'Use `import { "expect" } from "vitest";` instead.',
-      },
-      {
-        name: "describe",
-        message: 'Use `import { "describe" } from "vitest";` instead.',
-      },
-      {
-        name: "it",
-        message: 'Use `import { "it" } from "vitest";` instead.',
-      },
-      {
-        name: "test",
-        message: 'Use `import { "test" } from "vitest";` instead.',
-      },
-    ],
-  },
 };
