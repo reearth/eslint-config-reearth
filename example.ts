ea52e4e720c1b41fa95892449053ea61524495ec
rot1024 <aayhrot@gmail.com>
2024-05-27 20:51:08 +0900

    fix: fix typescript config

---
+++ example.ts
@@ -0,0 +1,3 @@
+// test linting typescriptt files
+
+export default {};
--- flat/typescript.mjs
+++ flat/typescript.mjs
@@ -1,22 +1,3 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import typescriptEslintParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 
-/** @type { import("eslint").Linter.FlatConfig[] } */
-export default [
-  {
-    files: ["**/*.{ts,mts,cts,tsx}"],
-    languageOptions: {
-      parser: typescriptEslintParser,
-    },
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-    },
-    rules: {
-      ...typescriptEslint.configs.recommended.rules,
-      ...typescriptEslint.configs["recommended-type-checked"].rules,
-      ...typescriptEslint.configs.strict.rules,
-      ...typescriptEslint.configs["strict-type-checked"].rules,
-      ...typescriptEslint.configs.stylistic.rules,
-    },
-  },
-];
+export default tseslint.config(...tseslint.configs.strict, ...tseslint.configs.stylistic);
